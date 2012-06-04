ed3ff550370fa74272e5b3bf4eeafca45484d87f
Andrew Scala <agscala@gmail.com>
2012-06-03 21:39:49 -0400

    Removing bad alert

--- client/user_prompt.js
+++ client/user_prompt.js
@@ -2,10 +2,7 @@ var username_prompt = function(callback) {
 	if(Session.get("user_id")) {
 		var id = Session.get("user_id");
 		if(id) {
-			console.log(id);
 			var user = Users.findOne({_id: id});
-			console.log(user);
-			alert(user.name);
 			if(user) return callback(user.name);
 		}
 	}
