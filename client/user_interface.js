9e5418cd8d0731ef6eb73e9e8dc6954f7d50ea04
Andrew Scala <agscala@gmail.com>
2012-06-03 20:16:29 -0400

    Finishing touches on the homepage

--- client/user_interface.js
+++ client/user_interface.js
@@ -1,5 +1,7 @@
 Meteor.startup(function() {
 	$(".your-calendars").live("click", function() {
 		$(".your-calendars-list").toggle();
+		$("#arrow-up").toggle();
+		$("#arrow-down").toggle();
 	});
 });
